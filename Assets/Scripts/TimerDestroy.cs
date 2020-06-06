8b14a8eaa6e966306ff698cc6dbc275c0fbacffb
Bhagyashree-Rawal <bhagyashreerawaal17@gmail.com>
2020-06-06 16:34:34 -0700

    Added timer to delete the line after sometime

---
+++ Assets/Scripts/TimerDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerDestroy : MonoBehaviour
+{	
+	public float INTERVAL=10; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject,INTERVAL);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
