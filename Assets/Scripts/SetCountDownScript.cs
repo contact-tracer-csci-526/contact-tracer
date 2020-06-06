f9dcc6cd92f87b3e1902524d8e1d26725a9558d8
pspatel04 <pspatel@usc.edu>
2020-06-06 15:11:26 -0700

    Added CountDown Before Game Starts

---
+++ Assets/Scripts/SetCountDownScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCountDownScript : MonoBehaviour
+{
+    private DelayedStartScript CDS;
+    
+    public void SetCountDownNow()
+    {
+       CDS = GameObject.Find ("DelayedStart").GetComponent<DelayedStartScript> ();
+        CDS.counterDownDone = true;
+        
+    }
+    
+}
