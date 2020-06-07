e17e08c7614edfcb27978b2483d7334865c45b8a
vkanduku1995 <54661552+vkanduku1995@users.noreply.github.com>
2020-06-07 14:42:51 -0400

    Splash Screen
    Splash Screen before the game starts

---
+++ Assets/Splash/SplashSequence.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashSequence : MonoBehaviour
+{
+    public static int SceneNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*if (SceneNumber == 0)
+        {
+            StartCoroutine(ToSplashTwo());
+        }*/
+        if (SceneNumber == 0)
+        {
+            StartCoroutine(ToMainMenu());
+        }
+
+    }
+
+    /*IEnumerator ToSplashTwo()
+    {
+        yield return new WaitForSeconds(2);
+        SceneNumber = 1;
+        SceneManager.LoadScene(1);
+    }*/
+
+    IEnumerator ToMainMenu()
+     {
+         yield return new WaitForSeconds(5);
+         SceneNumber = 1;
+         SceneManager.LoadScene(1);
+     }
+}
