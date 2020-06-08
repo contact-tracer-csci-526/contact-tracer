af3f52ea9c26a881cd20fe0a360f17c20dcf4f53
Elden <eldeniyenden@gmail.com>
2020-06-08 08:33:45 -0700

    refactor: divide BallScript into different behavior objects
    - BallScript is renamed as Ball.
    - Path `Balls/` is defined and it will have Ball-related files.
    - Sprites and Textures are moved to `Resources/`. This is in order to
      make Resources available to load dynamically.
    - Enum BallType is defined onto which which an initial state is injected
    through a configuration on Unity editor.
    - As stated, Sprites are made dynamically loaded. This change will take
      place in elsewhere of the project.

---
+++ Assets/Scripts/Balls/NormalBallBehavior.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class NormalBallBehavior : BallBehavior
+{
+    public static string TAG = "Ball";
+
+    public NormalBallBehavior(Ball ball)
+    {
+        this.ball = ball;
+    }
+
+    public override void HandleOnCollisionEnter2D(Collision2D other)
+    {
+        Transform transform = ball.transform;
+        SpriteRenderer currentSprite = ball.gameObject.GetComponent<SpriteRenderer>();
+        Ball otherBall = other.gameObject.GetComponent<Ball>();
+
+        if (otherBall != null) {
+            bool isVirusHit = otherBall.ballType == BallType.VIRUS;
+            if (isVirusHit) {
+                TransformsTo(BallType.VIRUS);
+            }
+        }
+    }
+}
