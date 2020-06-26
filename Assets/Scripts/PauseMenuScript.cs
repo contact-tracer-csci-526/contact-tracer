f0a58e8cb8d068b71db8acf60a7b33b124dda6a7
pspatel04 <pspatel@usc.edu>
2020-06-25 17:04:11 -0700

    Added Pause Button next to Timer
    On clicking pause button it opens Pause menu with Resume, Menu and Quit buttons

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+   // public static bool GamePause = false;
+
+    public GameObject pauseMenuUI, PauseButton;
+    
+
+
+    // Update is called once per frame
+   
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+        PauseButton.SetActive(true);
+       
+        //GamePause = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        PauseButton.SetActive(false);
+        Time.timeScale = 0f;
+        //GamePause = true;
+    }
+
+    public void LoadMenu()
+    {
+        //Debug.Log("Menu");
+        GameManager.CurrentGameState = GameManager.GameState.MenuRestart;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("menu");
+       
+        
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        //Debug.Log("Quit");
+        Application.Quit();
+    }
+}
