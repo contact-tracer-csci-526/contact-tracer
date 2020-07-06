ec4e4024e8de59ac45918b34990492bfa715d1a4
vkanduku1995 <54661552+vkanduku1995@users.noreply.github.com>
2020-07-06 15:50:17 -0400

    Story line and audio track
    - Story board, immediately after splash screen
    - Will add the story line for tutorial once that is developed
    - Till the scene which asks the user if he wants to play the game or follow tutorial
    - Added auido track

---
+++ Assets/Scripts/StoryBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryBoard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        StartCoroutine(ToNextScene());
+    }
+
+    IEnumerator ToNextScene()
+    {
+        yield return new WaitForSeconds(9); // Timer is for 9 seconds because the story timeline rolls for 9 seconds
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/Scripts/TutorialOrGameNavigator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* To be changed */
+public class TutorialOrGameNavigator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(ToNextScene());
+    }
+
+    IEnumerator ToNextScene()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(3);
+    }
+}
