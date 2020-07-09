67c4db392eafc3d5af5ae32f6a7e56b54e02cd18
vkanduku1995 <54661552+vkanduku1995@users.noreply.github.com>
2020-07-08 21:22:31 -0400

    Add Tutorial 1 storyline and audio track

---
+++ Assets/Scripts/Tutorial1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(ToTuorialOne());
+    }
+
+    IEnumerator ToTuorialOne()
+    {
+        yield return new WaitForSeconds(6); // Timer is for 9 seconds because the story timeline rolls for 9 seconds
+        SceneManager.LoadScene(5);
+    }
+}
