bd046e4ca328b4c3f7ba7a0aad8388f2548ebfe8
pspatel04 <pspatel@usc.edu>
2020-07-10 16:19:18 -0700

    Added Skip Button
    Skip button added to skip the tutorial story line.

---
+++ Assets/Scripts/SkipScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void Skip() 
+    {
+
+    
+    SceneManager.LoadScene((int) GameSceneId.GAME_SCENE);
+
+    }
+    
+}
