0deb49aef114cf5ef6bd7a790b8cdd9b09d82efb
Priya Patel <55060156+pspatel04@users.noreply.github.com>
2020-07-12 19:45:48 -0700

    Add mute button
    * Added mute button
    
    Left top corner. Added mute button to turn on/off music.
    
    * style: format document
    
    Co-authored-by: Elden <eldeniyenden@gmail.com>

---
+++ Assets/Scripts/MuteScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MuteScript : MonoBehaviour
+{
+    public Sprite musicOnImage;
+    public Sprite musicOffImage;
+    public GameObject MuteButton;
+    public bool isMute;
+
+    void Start() {
+        MuteButton = GameObject.Find("MuteButton");
+    }
+
+    public void Mute (){
+        isMute = !isMute;
+        AudioListener.volume = isMute ? 0 : 1;
+
+        if (isMute) {
+            MuteButton.GetComponent<Image>().sprite = musicOffImage;
+        } else {
+            MuteButton.GetComponent<Image>().sprite = musicOnImage;
+        }
+    }
+}
